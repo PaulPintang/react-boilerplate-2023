5fc6591a6ca0d7242a1af82a42f3c3e489470439
PaulPintang <pauljpintang@gmail.com>
2023-03-30 10:52:42 +0800

    feature: navlink router link ui

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,21 +1,53 @@
 import { Suspense } from "react";
 import { Container, Center, Flex, Text } from "@mantine/core";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+type Link = {
+  isActive: boolean;
+  isPending: boolean;
+};
 
 const RootLayout = () => {
   return (
     <Container>
       <Center py={20}>
         <Flex gap={20}>
-          <Link to="home" className="no-underline">
+          <NavLink
+            to="home"
+            className={({ isActive, isPending }: Link) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-green-500"
+                : "text-gray-400"
+            }
+          >
             <Text>HOME</Text>
-          </Link>
-          <Link to="contact/number" className="no-underline">
+          </NavLink>
+          <NavLink
+            to="contact/number"
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-green-500"
+                : "text-gray-400"
+            }
+          >
             <Text>CONTACT</Text>
-          </Link>
-          <Link to="about" className="no-underline">
+          </NavLink>
+          <NavLink
+            to="about"
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-green-500"
+                : "text-gray-400"
+            }
+          >
             <Text>ABOUT</Text>
-          </Link>
+          </NavLink>
         </Flex>
       </Center>
       <Suspense fallback={<p>Loading...</p>}>
